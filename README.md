c20fd21066b36cb00005e595f9424aa39c25c74c
DONEpreventivi <simone.nervo@gmail.com>
2026-07-12 17:03:09 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,30 +1,24 @@
-# Preventivi D.ONE v1.0
+# Preventivi D.ONE 2.0
 
-Pacchetto completo pronto per GitHub Pages.
+Versione completamente riorganizzata e modulare.
 
-## Caricamento
+## Funzioni
+- preventivo food + beverage;
+- barra recap sempre visibile;
+- archivio locale;
+- ricerca cliente;
+- duplicazione;
+- storico modifiche;
+- ripristino versioni;
+- impostazioni piatti e beverage;
+- PDF senza dati interni;
+- naming PDF automatico;
+- nuovo preventivo;
+- layout mobile ottimizzato.
 
-Sostituisci nella root del repository:
+## Pubblicazione
+Carica tutto il contenuto dello ZIP nella root del repository GitHub Pages, mantenendo le cartelle:
+- assets/
+- icons/
 
-- `index.html`
-- `manifest.webmanifest`
-- `sw.js`
-- `README.md`
-- cartella `icons`
-- cartella `images`
-
-## Aggiornamento su iPhone
-
-Dopo il commit:
-
-1. attendi 1-2 minuti;
-2. apri il link GitHub Pages in Safari;
-3. ricarica la pagina;
-4. chiudi completamente la web app;
-5. riaprila dall'icona Home.
-
-## Nome PDF
-
-Il titolo viene predisposto nel formato:
-
-`24-07-26_Carlotta_propostaA`
+Dopo il commit, attendi 1-2 minuti e aggiorna il link in Safari.
--- sw.js
+++ sw.js
@@ -1,34 +1,14 @@
-const CACHE="done-preventivi-v1-0";
+const CACHE="done-preventivi-v2-0";
 const FILES=[
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./images/logo-done.png",
-  "./icons/icon-180.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./","./index.html","./manifest.webmanifest",
+  "./assets/css/app.css","./assets/js/data.js","./assets/js/app.js",
+  "./assets/images/logo-done.png",
+  "./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"
 ];
-
-self.addEventListener("install",event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
-});
-
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET") return;
-  event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request).then(response=>response || caches.match("./index.html")))
-  );
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r})
+  .catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
 });
